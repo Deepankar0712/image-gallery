710f1c5230b4e69826fb28a9f1c37d0dcf03e64d
Deepankar0712 <sharmadeepankar0712@gmail.com>
2023-05-10 11:48:18 +0530

    image gallery

---
+++ script.js
@@ -0,0 +1,21 @@
+function generateImage(obj){
+				id=obj.id;
+				if(id==1){
+				  document.getElementById("myimage").src="pic1.png"
+				}
+				if(id==2){
+				  document.getElementById("myimage").src="pic2.png"
+				}
+				if(id==3){
+				  document.getElementById("myimage").src="pic3.png"
+				}
+				if(id==4){
+				  document.getElementById("myimage").src="pic4.png"
+				}
+				if(id==5){
+				  document.getElementById("myimage").src="pic5.png"
+				}
+				if(id==6){
+				  document.getElementById("myimage").src="pic6.png"
+				}
+}
